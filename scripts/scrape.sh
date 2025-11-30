62b82e667d134cbe5cf2e72212abd87ec2ef3a89
hengin-eer <hirokix0608@gmail.com>
2025-12-01 00:11:38 +0900

    🐛 元々あるスクレイプ用シェルでMCC対応のスクリプトを走らせる

--- scripts/scrape.sh
+++ scripts/scrape.sh
@@ -1,6 +1,10 @@
-year=2020
- node scripts/scrape.js $year M https://www.akashi.ac.jp/mechanical/m_curriculum
- node scripts/scrape.js $year ED https://www.akashi.ac.jp/electrical/e_curriculum
- node scripts/scrape.js $year EJ https://www.akashi.ac.jp/electrical/e_curriculum
- node scripts/scrape.js $year C https://www.akashi.ac.jp/civil/c_curriculum
- node scripts/scrape.js $year A https://www.akashi.ac.jp/architecture/a_curriculum
+#!/bin/bash
+# ./scripts/scrape.sh [year]
+
+year=${1:-2025}
+
+echo "Scraping curriculum data for year: $year"
+
+node scripts/scrape_mcc.js $year
+
+echo "All done!"
